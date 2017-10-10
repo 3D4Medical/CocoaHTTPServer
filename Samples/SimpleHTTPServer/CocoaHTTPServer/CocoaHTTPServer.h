0c6fae83aeb91b8bb41ee8c4e2724ee1ec55051f
Andrii Bohdanovskyi <andriy.bogdanovskii@3d4medical.com>
2017-10-10 14:39:18 +0100

    added framework build targets

---
+++ Samples/SimpleHTTPServer/CocoaHTTPServer/CocoaHTTPServer.h
@@ -0,0 +1,20 @@
+//
+//  CocoaHTTPServer.h
+//  CocoaHTTPServer
+//
+//  Created by Andrii Bohdanovskyi on 15/06/2016.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaHTTPServer.
+FOUNDATION_EXPORT double CocoaHTTPServerVersionNumber;
+
+//! Project version string for CocoaHTTPServer.
+FOUNDATION_EXPORT const unsigned char CocoaHTTPServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaHTTPServer/PublicHeader.h>
+#import <CocoaHTTPServer/HTTPServer.h>
+#import <CocoaHTTPServer/DDLog.h>
+#import <CocoaHTTPServer/DDTTYLogger.h>
